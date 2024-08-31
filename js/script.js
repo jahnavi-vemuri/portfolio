fe7e3ea04bf141f7c2a80b47302a77bdef58fd25
Jahnavi Vemuri <jahnavi.vemuri3801@gmail.com>
2024-08-31 09:02:42 -0400

    Dev completed

--- js/script.js
+++ js/script.js
@@ -36,3 +36,37 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+// Select all modal triggers and close buttons
+const modalTriggers = document.querySelectorAll('.img-card-container');
+const modalCloseBtns = document.querySelectorAll('.modal-close');
+
+// Function to open the modal
+modalTriggers.forEach(trigger => {
+  trigger.addEventListener('click', function() {
+    const modalId = this.id.replace('_open', '');
+    document.getElementById(`modal-${modalId}`).style.display = 'flex';
+    document.body.classList.add('modal-open');
+  });
+});
+
+// Function to close the modal
+modalCloseBtns.forEach(closeBtn => {
+  closeBtn.addEventListener('click', function() {
+    const modalId = this.getAttribute('data-target');
+    document.getElementById(modalId).style.display = 'none';
+    document.body.classList.remove('modal-open');
+  });
+});
+
+// Close modal on clicking outside
+window.addEventListener('click', function(event) {
+  const openModals = document.querySelectorAll('.modal-container[style="display: flex;"]');
+  openModals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+      document.body.classList.remove('modal-open');
+    }
+  });
+});
